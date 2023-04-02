12f0ac89a083f2e3df5f60a572862bd03d93b8dd
MQ <mq@4mq.xyz>
2023-04-02 20:13:36 +0200

    add flags

--- app/src/main/java/cz/ujep/ki/mconverter/CurrencyRecyclerViewAdapter.java
+++ app/src/main/java/cz/ujep/ki/mconverter/CurrencyRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import static androidx.core.content.ContextCompat.startActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,13 +16,49 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.HashMap;
+
 public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyViewHolder> {
     private Cursor mc;
     private AppCompatActivity parent;
+    private HashMap<String, String> currencyToCountryMap;
 
     public CurrencyRecyclerViewAdapter(Cursor cursor, AppCompatActivity parent) {
         this.mc = cursor;
         this.parent = parent;
+        currencyToCountryMap = new HashMap<>();
+        currencyToCountryMap.put("AUD", "AU");
+        currencyToCountryMap.put("BRL", "BR");
+        currencyToCountryMap.put("BGN", "BG");
+        currencyToCountryMap.put("CNY", "CN");
+        currencyToCountryMap.put("DKK", "DK");
+        currencyToCountryMap.put("EUR", "EU");
+        currencyToCountryMap.put("PHP", "PH");
+        currencyToCountryMap.put("HKD", "HK");
+        currencyToCountryMap.put("INR", "IN");
+        currencyToCountryMap.put("IDR", "ID");
+        currencyToCountryMap.put("ISK", "IS");
+        currencyToCountryMap.put("ILS", "IL");
+        currencyToCountryMap.put("JPY", "JP");
+        currencyToCountryMap.put("ZAR", "ZA");
+        currencyToCountryMap.put("CAD", "CA");
+        currencyToCountryMap.put("KRW", "KR");
+        currencyToCountryMap.put("HUF", "HU");
+        currencyToCountryMap.put("MYR", "MY");
+        currencyToCountryMap.put("MXN", "MX");
+        //currencyToCountryMap.put("XDR", "MMF");
+        currencyToCountryMap.put("NOK", "NO");
+        currencyToCountryMap.put("NZD", "NZ");
+        currencyToCountryMap.put("PLN", "PL");
+        currencyToCountryMap.put("RON", "RO");
+        currencyToCountryMap.put("SGD", "SG");
+        currencyToCountryMap.put("SEK", "SE");
+        currencyToCountryMap.put("CHF", "CH");
+        currencyToCountryMap.put("THB", "TH");
+        currencyToCountryMap.put("TRY", "TR");
+        currencyToCountryMap.put("USD", "US");
+        currencyToCountryMap.put("GBP", "GB");
+        currencyToCountryMap.put("CZK", "CZ");
     }
 
     @NonNull
@@ -42,7 +79,17 @@ public class CurrencyRecyclerViewAdapter extends RecyclerView.Adapter<CurrencyVi
         double amount = mc.getDouble(mc.getColumnIndex(CurrencyContentProvider.AMOUNT));
         double rate = mc.getDouble(mc.getColumnIndex(CurrencyContentProvider.RATE));
 
-        holder.bind(name, code, amount, rate);
+        Resources resources = parent.getResources();
+        String countryCode = currencyToCountryMap.get(code);
+        int flagId = 0;
+        if (countryCode != null) {
+            flagId = resources.getIdentifier(countryCode.toLowerCase(), "drawable", parent.getPackageName());
+        }
+
+        if (flagId == 0)
+            flagId = R.drawable.ic_launcher_foreground;
+
+        holder.bind(name, code, amount, rate, flagId);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/cz/ujep/ki/mconverter/CurrencyViewHolder.java
+++ app/src/main/java/cz/ujep/ki/mconverter/CurrencyViewHolder.java
@@ -1,10 +1,12 @@
 package cz.ujep.ki.mconverter;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 public class CurrencyViewHolder extends RecyclerView.ViewHolder {
@@ -12,6 +14,8 @@ public class CurrencyViewHolder extends RecyclerView.ViewHolder {
     private TextView codeTextView;
     private TextView amountTextView;
     private TextView rateTextView;
+    private ImageView flagView;
+
 
     public CurrencyViewHolder(View itemView) {
         super(itemView);
@@ -19,12 +23,16 @@ public class CurrencyViewHolder extends RecyclerView.ViewHolder {
         codeTextView = itemView.findViewById(R.id.itemCode);
         amountTextView = itemView.findViewById(R.id.itemAmount);
         rateTextView = itemView.findViewById(R.id.itemRate);
+        flagView = itemView.findViewById(R.id.itemFlag);
+
+
     }
 
-    public void bind(String name, String code, double amount, double rate) {
+    public void bind(String name, String code, double amount, double rate, int flagId) {
         nameTextView.setText(name);
         codeTextView.setText(code);
         amountTextView.setText(String.format(Locale.getDefault(), "%.2f", amount));
         rateTextView.setText(String.format(Locale.getDefault(), "%.2f", rate));
+        flagView.setImageResource(flagId);
     }
 }
